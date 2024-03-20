8036f2f8a9f7daf66f711669a3474db080c5c029
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-20 11:23:29 +0530

    change java file to kotlin

---
+++ mylibrary/src/main/java/com/example/mylibrary/Multiply.kt
@@ -0,0 +1,11 @@
+package com.example.mylibrary
+
+class Multiply {
+    fun printTheMessage(message: String) {
+        println("This is printed from android libray $message")
+    }
+
+    fun multiply(a: Int, b: Int) {
+        println("This result of this is ${a * b}")
+    }
+}
