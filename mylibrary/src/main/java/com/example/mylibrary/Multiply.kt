e267ba37ef923d4f189b88a62fd3c45334965e87
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-20 11:38:57 +0530

    add companion object

--- mylibrary/src/main/java/com/example/mylibrary/Multiply.kt
+++ mylibrary/src/main/java/com/example/mylibrary/Multiply.kt
@@ -1,11 +1,15 @@
 package com.example.mylibrary
 
 class Multiply {
-    fun printTheMessage(message: String) {
-        println("This is printed from android libray $message")
-    }
 
-    fun multiply(a: Int, b: Int) {
-        println("This result of this is ${a * b}")
+    companion object {
+        fun printTheMessage(message: String) {
+            println("This is printed from android libray $message")
+        }
+
+        fun multiply(a: Int, b: Int) {
+            println("This result of this is ${a * b}")
+        }
     }
 }
+
